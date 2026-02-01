c4433aad7442500b601a7fd4ad486fa440c7835a
MythinkIndie <mythinkindie@gmail.com>
2026-02-01 20:40:22 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  vite: {
-    optimizeDeps: {
-      exclude: ['better-sqlite3']
-    }
-  },
-  site: "https://abstract-game-design-system.vercel.app/",
+  output: "server",
   adapter: vercel(),
-  output: 'server',
-  server: {
-    port: 4321,
-    host: true
-  }
+  site: "https://abstract-game-design-system.vercel.app/",
 });
