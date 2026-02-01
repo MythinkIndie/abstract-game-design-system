17f5fd40888e5b653fa96d03b927b313a13f1a0c
MythinkIndie <mythinkindie@gmail.com>
2026-02-01 21:03:49 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import tailwind from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel(),
-  vite: {
-    plugins: [tailwind()]
-  }
 });
